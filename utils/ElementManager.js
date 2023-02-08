5e0ffa3973bfdbe3dc465ce00474d039a598e3de
Faarie <jonjondm2001@gmail.com>
2023-02-08 10:30:01 -0330

    Committing Desynced Repo. Oopsies
    Forgot to commit from home so I had to commit through file upload. >.>

---
+++ utils/ElementManager.js
@@ -0,0 +1,19 @@
+function divFadeOut(elementID) {
+    const element = document.getElementById(elementID);
+    element.style.transition = "opacity 0.3s ease-in-out, top 0.5s ease-in-out";
+    element.style.opacity = 0;
+    element.style.top = "-50%";
+    setTimeout(() => {
+        element.style.display = "none";
+    }, 300);
+}
+
+function divFadeIn(elementID) {
+    const element = document.getElementById(elementID);
+    element.style.transition = "opacity 0.3s ease-in-out, top 0.5s ease-in-out";
+    element.style.opacity = 100;
+    element.style.top = "150%";
+    setTimeout(() => {
+        element.style.display = "none";
+    }, 300);
+}
---
+++ utils/PageManager.js
@@ -0,0 +1,15 @@
+function createSignIn() {
+    const signInPage = document.body.getElementById("SignInPage");
+    element.setProperty("id", "SignIn");
+    const text1 = document.createElement("input");
+    text1.type = "textbox";
+    const text2 = document.createElement("input");
+    text2.type = "textbox";
+    const btn = document.createElement("input");
+    btn.type = "text";
+    btn.onclick = "createRegister()"
+}
+
+function createRegister() {
+
+}
